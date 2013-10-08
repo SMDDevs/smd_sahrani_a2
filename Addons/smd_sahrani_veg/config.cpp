9b217168f4e38340be88fdbedcbd6e500038574c
NonovUrbizniz <dayzsahranihelp@gmail.com>
2013-10-07 22:38:41 -0400

    smd_sahrani_veg

---
+++ Addons/smd_sahrani_veg/config.cpp
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: Produced from mikero's Dos Tools Dll version 3.52
+//http://dev-heaven.net/projects/list_files/mikero-pbodll
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+//Class smd_sahrani_veg : config.bin{
+class CfgPatches
+{
+ class smd_sahrani_veg
+ {
+  units[] = {""};
+  weapons[] = {};
+  requiredAddons[] = {};
+ };
+};
+//};
